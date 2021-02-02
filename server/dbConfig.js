7388ef4180bbc71453027905519978d2d7f1f52d
Nic Barnes <colorpulse@gmail.com>
2021-02-02 11:55:07 +0100

    heroku test

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -6,7 +6,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 // const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-const connectionString = 'postgres://vkfarmfyscncmo:c23be9c3ac6c9d2086a5357906f40c6aa1cca34171cafbec3805070d72c25f40@ec2-54-155-99-116.eu-west-1.compute.amazonaws.com:5432/d6sameouvbeqlo'
+const connectionString = `${process.env.HEROKU_POSTGRES_DB_URI}`
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
