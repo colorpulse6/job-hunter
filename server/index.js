05ccf9807b0625acdc18a4c8f79a79a6b09af618
Nic Barnes <colorpulse@gmail.com>
2021-02-12 11:20:04 +0100

    cors?

--- server/index.js
+++ server/index.js
@@ -9,6 +9,11 @@ const urlMetadata = require('url-metadata')
 
 //Middleware
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://job-toast.herokuapp.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(
   cors({
     origin: ["http://localhost:3000"],
