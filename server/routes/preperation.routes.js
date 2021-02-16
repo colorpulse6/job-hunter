3ac9e02b1e5282cff70c1b4ee6057f798783937b
Nic Barnes <colorpulse@gmail.com>
2021-02-16 11:41:15 +0100

    fixed cloudinary upload for heroku

--- server/routes/preperation.routes.js
+++ server/routes/preperation.routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { pool } = require("../dbConfig");
 const { isLoggedIn } = require("../helpers/auth-helper");
-var Meta = require('html-metadata-parser');
 
 const {
   getData,
